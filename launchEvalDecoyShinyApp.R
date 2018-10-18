7cce0892690a988ad13182ea76aaec92612d4eaf
lieven clement <lieven.clement@ugent.be>
2018-10-18 13:04:21 +0200

    local paths adjusted

--- launchEvalDecoyShinyApp.R
+++ launchEvalDecoyShinyApp.R
@@ -1,4 +1,7 @@
+#Line below has to be commented out when users are not working with Rstudio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 maxFileSize=100
 options(shiny.maxRequestSize=maxFileSize*1024^2)
 options(shiny.launch.browser = .rs.invokeShinyWindowExternal)
-shiny::runApp("~/App-evalDecoys/")
+shiny::runApp("App-evalDecoys/")
--- launchMSqRobShinyApp.R
+++ launchMSqRobShinyApp.R
@@ -1,5 +1,8 @@
+#Line below has to be commented out when users are not working with Rstudio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 maxFileSize=100
 options(shiny.maxRequestSize=maxFileSize*1024^2)
 options(shiny.launch.browser = .rs.invokeShinyWindowExternal)
-source("~/App-MSqRob/combineFeatures.R")
-shiny::runApp("~/App-MSqRob/")
+source("App-MSqRob/combineFeatures.R")
+shiny::runApp("App-MSqRob/")
